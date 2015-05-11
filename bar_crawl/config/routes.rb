742fcf8aa0fece907907f90d135c29c92449a8c3
Annapurna Singh <annapurna.singh@gmail.com>
2015-05-11 12:47:46 -0500

    RailsShell

--- bar_crawl/config/routes.rb
+++ bar_crawl/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :drinkers
+
+  get 'drinkers/index'
+
+  get 'drinkers/new'
+
+  get 'drinkers/show'
+
+  get 'drinkers/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
