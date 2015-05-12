496b522c9e3ee5a01775cf1b56b88ac7479890aa
Matt Lissner <mlissne@gmail.com>
2015-05-12 10:48:49 -0500

    move search to model from controller

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,27 +1,27 @@
 class BarsController < ApplicationController
 
-  def power(num, pow)
-    num ** pow
-  end
-
-  def haversine(lat1, long1, lat2, long2)
-    dtor = Math::PI/180
-    r = 3959
-
-    rlat1 = lat1 * dtor
-    rlong1 = long1 * dtor
-    rlat2 = lat2 * dtor
-    rlong2 = long2 * dtor
-
-    dlon = rlong1 - rlong2
-    dlat = rlat1 - rlat2
-
-    a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
-    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
-    d = r * c
-
-    return d
-  end
+  # def power(num, pow)
+  #   num ** pow
+  # end
+  #
+  # def haversine(lat1, long1, lat2, long2)
+  #   dtor = Math::PI/180
+  #   r = 3959
+  #
+  #   rlat1 = lat1 * dtor
+  #   rlong1 = long1 * dtor
+  #   rlat2 = lat2 * dtor
+  #   rlong2 = long2 * dtor
+  #
+  #   dlon = rlong1 - rlong2
+  #   dlat = rlat1 - rlat2
+  #
+  #   a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
+  #   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+  #   d = r * c
+  #
+  #   return d
+  # end
 
 
   def welcome
@@ -44,7 +44,13 @@ class BarsController < ApplicationController
       end
     end
 
-    render json: Bar.find(bar_id)
+    bar = Bar.find(bar_id)
+
+    data = {}
+    data[:bar] = bar
+    data[:closest_bars] = bar.closest_bars( 3 )
+
+    render json: data
 
   end
 
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -7,6 +7,11 @@ class Bar < ActiveRecord::Base
   end
 
 
+  def self.search(lat, long, num_bars=1)
+    # perform the search using the Math
+    # return all the bars found    
+  end
+
   def to_s
     self.name
   end
@@ -19,3 +24,5 @@ end
 #   has_many :cows, foreign_key: :farmer_id
 # end
 # Assume a cows includes a farmer_id
+
+# Bar.search(41, -87, 4)
